4d6774d32f4471ad6e4efd9bca9d68d8a7474b95
JJ_gum <jakub.janicki@gum.gov.pl>
2024-07-22 11:57:06 +0200

    wersja poniedzialek rano do zrobienia idk

--- myapp/admin.py
+++ myapp/admin.py
@@ -2,39 +2,66 @@ from django.contrib import admin
 from django.http import HttpResponse
 from .models import FormData
 from docx import Document
+from docx.enum.text import WD_ALIGN_PARAGRAPH
 import re
+
 
 def generate_docx(modeladmin, request, queryset):
     for form_data in queryset:
-        document = Document()
-        document.add_heading('User Input', 0)
-        document.add_paragraph(f"Nr zgłoszenia: {form_data.nr_zgloszenia}")
-        document.add_paragraph(f"Data zgłoszenia: {form_data.data_zgloszenia}")
-        document.add_paragraph(f"Nazwa zakładu: {form_data.nazwa_zakladu}")
-        document.add_paragraph(f"Laboratorium: {form_data.laboratorium}")
-        document.add_paragraph(f"Zgłaszający: {form_data.zglaszajacy}")
-        document.add_paragraph(f"Telefon: {form_data.telefon}")
-        document.add_paragraph(f"Nr pomieszczenia: {form_data.nr_pomieszczenia}")
-        document.add_paragraph(f"Nazwa urządzenia: {form_data.nazwa_urzadzenia}")
-        document.add_paragraph(f"Dostęp do sieci: {'Yes' if form_data.dostep_do_sieci else 'No'}")
-        document.add_paragraph(f"Nr ewidencyjny: {form_data.nr_ewidencyjny}")
-        document.add_paragraph(f"Nr gniazdka LAN: {form_data.nr_gniazdka_lan}")
-        document.add_paragraph(f"Opis zgłoszenia: {form_data.opis_zgloszenia}")
-        document.add_paragraph(f"Oczekiwania: {form_data.oczekiwania}")
-        document.add_paragraph(f"Istotność poufności: {form_data.istotnosc_pouf}")
-        document.add_paragraph(f"Istotność integralności: {form_data.istotnosc_integr}")
-        document.add_paragraph(f"Istotność dostępności: {form_data.istotnosc_dost}")
-        document.add_paragraph(f"Kierownik LIM opinia: {form_data.kierownik_lim_opinia}")
-        document.add_paragraph(f"Kierownik LIM podpis: {form_data.kierownik_lim_podpis}")
-        document.add_paragraph(f"Kierownik LIM data: {form_data.kierownik_lim_data}")
-        document.add_paragraph(f"Kierownik KM opinia: {form_data.kierownik_km_opinia}")
-        document.add_paragraph(f"Kierownik KM podpis: {form_data.kierownik_km_podpis}")
-        document.add_paragraph(f"Kierownik KM data: {form_data.kierownik_km_data}")
-        document.add_paragraph(f"Realizacja opis: {form_data.realizacja_opis}")
-        document.add_paragraph(f"Realizacja podpis: {form_data.realizacja_podpis}")
-        document.add_paragraph(f"Realizacja data: {form_data.realizacja_data}")
-        document.add_paragraph(f"Potwierdzenie podpis: {form_data.potwierdzenie_podpis}")
-        document.add_paragraph(f"Potwierdzenie data: {form_data.potwierdzenie_data}")
+        document = Document("C:\Projects\myproject\myapp\Zgloszenie_szablon.docx")
+        table1 = document.tables[0]
+        table2 = document.tables[1]
+        # Tabela 1
+        table1.cell(0, 3).paragraphs[0].text = form_data.nr_zgloszenia or ()
+        table1.cell(0, 3).add_paragraph().alignment = WD_ALIGN_PARAGRAPH.CENTER
+        table1.cell(0, 3).paragraphs[1].text = form_data.nr_EZD or ()
+        if str(form_data.data_zgloszenia) == "None":
+            table1.cell(1, 3).paragraphs[-1].text = ""
+        else:
+            table1.cell(1, 3).paragraphs[-1].text = str(form_data.data_zgloszenia)
+        # Paragrafy
+        document.paragraphs[2].add_run(form_data.nazwa_zakladu or ())
+        document.paragraphs[3].add_run(form_data.laboratorium or ())
+        document.paragraphs[4].add_run(form_data.zglaszajacy or ())
+        document.paragraphs[5].add_run(form_data.telefon or ())
+        document.paragraphs[6].add_run(form_data.nr_pomieszczenia or ())
+
+        # Tabela 2
+        table2.cell(0, 0).paragraphs[-1].add_run(form_data.nazwa_urzadzenia) or ()
+        table2.cell(1, 0).paragraphs[-1].add_run(form_data.dostep_do_sieci or ())
+        table2.cell(2, 0).paragraphs[-1].add_run(form_data.nr_ewidencyjny or ())
+        table2.cell(2, 2).paragraphs[-1].add_run(form_data.nr_gniazdka_lan or ())
+        table2.cell(3, 0).paragraphs[-1].add_run(form_data.opis_zgloszenia or ())
+        table2.cell(4, 0).paragraphs[-1].add_run(form_data.oczekiwania or ())
+        table2.cell(5, 3).text = form_data.istotnosc_pouf or ""
+        table2.cell(6, 3).text = form_data.istotnosc_integr or ""
+        table2.cell(7, 3).text = form_data.istotnosc_dost or ""
+        table2.cell(9, 0).paragraphs[0].text = form_data.zglaszajacy_podpis or ""
+        table2.cell(10, 0).paragraphs[0].add_run(form_data.kierownik_lim_opinia or "")
+        table2.cell(11, 0).paragraphs[0].text = form_data.kierownik_lim_podpis or ""
+        if str(form_data.kierownik_lim_data) == "None":
+            table2.cell(11, 0).paragraphs[2].text = ""
+        else:
+            table2.cell(11, 0).paragraphs[2].text = str(form_data.kierownik_lim_data)
+
+        table2.cell(13, 0).paragraphs[1].add_run(form_data.kierownik_km_opinia or "")
+        table2.cell(13, 0).paragraphs[0].add_run(form_data.kierownik_km_podpis or "")
+        if str(form_data.kierownik_km_data) == "None":
+            table2.cell(13, 0).paragraphs[-1].text = ""
+        else:
+            table2.cell(13, 0).paragraphs[-1].text = str(form_data.kierownik_km_data)
+        table2.cell(14, 0).paragraphs[0].add_run(form_data.realizacja_opis or "")
+        table2.cell(15, 0).paragraphs[-1].text = form_data.realizacja_podpis or ""
+        if str(form_data.realizacja_data) == "None":
+            table2.cell(15, 0).paragraphs[-1].text = ""
+        else:
+            table2.cell(15, 0).paragraphs[-1].text = str(form_data.realizacja_data)
+
+        table2.cell(16, 0).paragraphs[2].add_run(form_data.potwierdzenie_podpis or "")
+        if str(form_data.potwierdzenie_data) == "None":
+            table2.cell(16, 0).paragraphs[-1].text = ""
+        else:
+            table2.cell(16, 0).paragraphs[-1].text = str(form_data.potwierdzenie_data)
 
         # Clean the filename to avoid illegal characters
         cleaned_name = re.sub(r'[\\/*?:"<>|]', "", form_data.nr_zgloszenia)
@@ -43,7 +70,9 @@ def generate_docx(modeladmin, request, queryset):
         document.save(response)
         return response
 
+
 generate_docx.short_description = "Generate DOCX for selected entries"
+
 
 @admin.register(FormData)
 class FormDataAdmin(admin.ModelAdmin):
--- myproject/settings.py
+++ myproject/settings.py
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/warsaw'
 
 USE_I18N = True
 
