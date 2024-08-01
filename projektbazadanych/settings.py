426bee9cfad37ecb3a554a2d442edb30b51f7e52
JJ_gum <jakub.janicki@gum.gov.pl>
2024-08-01 15:03:32 +0200

    wersja typu czwartek

--- projektbazadanych/settings.py
+++ projektbazadanych/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-y)^-x73=q&-3e7yoq7nh8!)p^fmj)z&w#9=q^lee*!!7*_4t3-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.200.202', 'localhost', '127.0.0.1']
 
 
 # Application definition
